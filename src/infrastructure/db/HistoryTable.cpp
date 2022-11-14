455d6061c397d5d9633cb2e0a8ff3025caee99c0
zhuzichu <zhuzichu520@gmail.com>
2022-11-14 12:07:03 +0800

    update

---
+++ src/infrastructure/db/HistoryTable.cpp
@@ -0,0 +1,36 @@
+﻿#include "HistoryTable.h"
+
+
+HistoryTable::HistoryTable(QObject *parent) : Nut::Table(parent)
+{
+    init();
+}
+
+int HistoryTable::id() const
+{
+    return m_id;
+}
+
+QString HistoryTable::name() const
+{
+    return m_name;
+}
+
+
+void HistoryTable::setId(int id)
+{
+    if (m_id == id)
+        return;
+
+    m_id = id;
+    Q_EMIT idChanged(m_id);
+}
+
+void HistoryTable::setName(QString name)
+{
+    if (m_name == name)
+        return;
+
+    m_name = name;
+    Q_EMIT nameChanged(m_name);
+}
---
+++ src/infrastructure/db/HistoryTable.h
@@ -0,0 +1,37 @@
+﻿#ifndef HISTORYTABLE_H
+#define HISTORYTABLE_H
+
+#include <QObject>
+#include <QtNut/table.h>
+
+class HistoryTable : public Nut::Table
+{
+    Q_OBJECT
+    Q_PROPERTY(int id READ id WRITE setId NOTIFY idChanged)
+    Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
+
+    int m_id;
+    QString m_name;
+
+    // BEGIN OF NUT MACROS
+    NUT_PRIMARY_KEY(id)
+    NUT_FIELD(int, id)
+    NUT_FIELD(QString, name)
+    //END OF NUT MACROS
+
+public:
+    explicit HistoryTable(QObject *parent = nullptr);
+
+    int id() const;
+    QString name() const;
+
+public Q_SLOTS:
+    void setId(int id);
+    void setName(QString name);
+
+Q_SIGNALS:
+    void idChanged(int id);
+    void nameChanged(QString name);
+};
+
+#endif // HISTORYTABLE_H
